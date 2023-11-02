e1b0218135400ba6b2c87cf4a17c17958ef7ed62
shireeshageddam <shireeshageddam@gmail.com>
2023-11-02 18:42:00 +0530

    commit

---
+++ day 2/index.js
@@ -0,0 +1,34 @@
+let msg ="hello"
+let completeMsg=msg+str1;
+console.log(completeMsg);
+
+let str1="siri"
+let str2=`hello,${str1}`;
+
+
+let isUser= true;
+let arr1=[10,20,30];
+
+let obj={
+    name: "siri",
+    city: "hyd"
+
+}
+
+
+var a;
+console.log(a);//undefined
+
+a=10;
+console.log(a)//10
+
+
+console.log("10" + "40");   // 1040
+console.log(6 - 5);     //1 
+console.log("Rohan" + "Rao");  // Rohan Rao
+console.log(" "+"0");         // 0
+console.log("Ramesh" - "Rao");   // NaN
+console.log(true+true);   //2
+console.log(true+ false); // 1 
+console.log(false+true);  // 1
+console.log(false-true); //  -1
